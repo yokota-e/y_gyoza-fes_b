f123fd55b5140295128db85fadc195f0ab33319a
Emiko Kishimoto <emiko.markz.school@gmail.com>
2026-02-26 16:05:56 +0900

    nav_bar.phpの内容追加

---
+++ common/nav_var.php
@@ -0,0 +1,30 @@
+
+<div id="top" class="c-top">
+            <div class="l-wrapper c-top__contents">
+                <h1 class="c-logo">
+                    <a href="index.html">
+                        <picture>
+                            <source srcset="./img/logo_02.svg" media="(min-width: 768px)">
+                            <img src="./img/logo_line_02.svg" alt="ふくおか餃子FES">
+                        </picture>
+                    </a>
+                </h1>
+                <nav class="c-header-nav">
+                    <ul class="c-header-nav__list">
+                        <li class="c-header-nav__item">
+                            <a href="./index.html">Home</a>
+                        </li>
+                        <li class="c-header-nav__item">
+                            <a href="./shop-list.html">店舗一覧</a>
+                        </li>
+                        <li class="c-header-nav__item">
+                            <a href="./faq.html">よくある質問</a>
+                        </li>
+                        <li class="c-header-nav__item">
+                            <a href="./form.html">お問い合わせ</a>
+                        </li>
+                    </ul>
+                    <img class="c-header-nav__hamburger" src="./img/icon_hamburger.png" alt="">
+                </nav>
+            </div>
+        </div>
