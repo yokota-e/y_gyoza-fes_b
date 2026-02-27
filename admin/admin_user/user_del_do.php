0c96a98b15128afdeeeac376826b6edb182a0483
Emiko Kishimoto <emiko.markz.school@gmail.com>
2026-02-27 16:50:25 +0900

    ユーザー削除機能完成

--- admin/admin_user/user_del_do.php
+++ admin/admin_user/user_del_do.php
@@ -1,28 +1,30 @@
 <?php
 require_once __DIR__ . '/../../function/function.php';
+require_once __DIR__ . '/../../common/login_check.php';
 
 // TODO: データ受け取り
-if (!empty($_POST)) {
+if (!empty($_SESSION)) {
     // POST送信されたとき
-    if (!empty($_POST['id'])) {
+    if (!empty($_SESSION['id'])) {
         // TODO: idのチェック（空の場合）
-        $id = $_POST['id'];
+        $id = $_SESSION['id'];
+      
         // DBに接続
         try {
             $db = db_connect();
             // usersテーブルから1行削除するSQL
-            $sql = 'UPDATE users SET is_delited= 1';
+            $sql = 'UPDATE users SET is_deleted= 1 WHERE id = :id';
             $stmt = $db->prepare($sql);
             // idをプレースホルダへバインド
             $stmt->bindParam(':id', $id, PDO::PARAM_INT);
             $stmt->execute();
 
             // トップページへ画面遷移
-            header('location:admin.php');
+            header('location:../admin.php');
             exit();
         } catch (PDOException $e) {
             exit('エラー: '.$e->getMessage());
         }
     }
 }
-// header('location:../admin.php');
+header('location:../admin.php');
