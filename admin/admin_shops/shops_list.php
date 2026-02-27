9317b5624faa79346738f7c4fc2520963769a570
Erika Yokota <rin0000ki@gmail.com>
2026-02-27 11:39:05 +0900

    管理系コード微調整・管理用店舗一覧ページ更新

--- admin/admin_shops/shops_list.php
+++ admin/admin_shops/shops_list.php
@@ -1,17 +1,34 @@
 <?php
-// require_once __DIR__ . '../../function/function.php';
+require_once __DIR__ . '/../../function/function.php';
+require_once __DIR__ . '/../../common/login_check.php';
 
-session_start();
+
+// ログインしてる人用
+try {
+    // DBへ接続
+    $db = db_connect();
+    // プリペアードステートメント作成
+    $sql = 'SELECT id,name FROM shops ORDER BY id ASC';
+    $stmt = $db->prepare($sql);
+
+
+    // SQLの実行
+    $stmt->execute();
+
+    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+} catch (PDOException $e) {
+    exit('エラー:' . $e->getMessage());
+}
 ?>
-
-
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>店舗一覧</title>
+    <title>【管理用】店舗一覧</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./css/style.css">
 </head>
 
 <body>
@@ -19,12 +36,14 @@ session_start();
     <h1>店舗一覧</h1>
 
     <ul>
-        <?php ?>
-        <li><a href="shops_detail.php?id=<?php echo 'id' ?>"><?php echo 'name' ?></a></li>
-        <?php ?>
-
-
+        <?php foreach ($result as $shop): ?>
+            <li><a href="shops_detail.php?id=<?php echo $shop['id'] ?>"><?php echo $shop['name'] ?></a></li>
+        <?php endforeach; ?>
     </ul>
+    <div class="d-block">
+        <a href="shops_add.php" class="btn btn-outline-primary mb-5">店舗を追加する</a>
+    </div>
+    <a href="../admin.php" class="btn btn-primary">管理者TOPに戻る</a>
 </body>
 
 </html>
