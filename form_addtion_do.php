dda787824dfc8208b7e685e8da46a58b1f55f16a
Komatsu <komayoshi934@gmail.com>
2026-03-02 11:24:05 +0900

    insert文完成

--- form_addtion_do.php
+++ form_addtion_do.php
@@ -1,16 +1,16 @@
 <?php
 include('./function/function.php');
 
-$role = $_POST["inquiry-type"];
-echo $role;
-$name = $_POST["user-name"];
-$name_kana = $_POST["user-name-furigana"];
-$company = $_POST["company-name"];
-$tel = $_POST["telephone-number"];
-$address = $_POST["user-email-address"];
-$shop_name = $_POST["store-name"];
-$text_body = $_POST["inquiry-details"];
-
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+$role = $_POST["type"];
+$name = $_POST["name"];
+$name_kana = $_POST["name_kana"];
+$company = $_POST["company"];
+$tel = $_POST["tel"];
+$address = $_POST["address"];
+$shop_name = $_POST["store"];
+$text_body = $_POST["text"];
+}
 //必須項目チェック
 if ($role == '' || $name == '' || $name_kana == '' ||  $company == '' || $tel == '' || $address == '' || $shop_name == '' || $text_body == '') {
     // header("Location: form_addition.php");
@@ -21,7 +21,7 @@ try {
     //sDBへ接続
     $db = db_connect();
     //プリペアードステートメント作成
-    $sql = 'INSERT INTO contacts(`role`, `name`, `name_kana`, `company`, `tel`, `address`, `shop_name`, `body`,now()) VALUES (:role,:name,:name_kana,:company,:tel,:address,:shop_name,:body,now())';
+    $sql = 'INSERT INTO contacts(`role`, `name`, `name_kana`, `company`, `tel`, `address`, `shop_name`, `body`,`post_date`) VALUES (:role,:name,:name_kana,:company,:tel,:address,:shop_name,:body,now())';
     $stmt = $db->prepare($sql);
     //$stmt->bindParam（’プレースホルダ’、プレースホルダに埋め込みたい値、データ型）
     $stmt->bindParam(':role', $role, PDO::PARAM_INT);
@@ -31,7 +31,7 @@ try {
     $stmt->bindParam(':tel', $tel, PDO::PARAM_STR);
     $stmt->bindParam(':address', $address, PDO::PARAM_STR);
     $stmt->bindParam(':shop_name', $shop_name, PDO::PARAM_STR);
-    $stmt->bindParam(':body', $body, PDO::PARAM_STR);
+    $stmt->bindParam(':body', $text_body, PDO::PARAM_STR);
     //SQL実行
     $stmt->execute();
 } catch (PDOException $e) {
--- function/function.php
+++ function/function.php
@@ -1,10 +1,10 @@
 <?php
 
 // DBへの接続情報
-define('DB_HOST','localhost');
-define('DB_NAME','gyoza');
-define('DB_USER','gyozauser');
-define('DB_PASS','password');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'gyoza');
+define('DB_USER', 'gyozauser');
+define('DB_PASS', 'password');
 
 // DBへ接続する関数
 function db_connect()
@@ -13,4 +13,28 @@ function db_connect()
     $db = new PDO($dsn, DB_USER, DB_PASS);
     $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
     return $db;
+}
+
+//管理者IDから管理者名を返すID
+function get_users_list()
+{
+    $roles = array();
+    try {
+        //rolesテーブルから全レコードを取得
+        $db = db_connect();
+        $sql = 'SELECT * FROM roles';
+        $stmt = $db->prepare($sql);
+        $stmt->execute();
+
+        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        // debug_check_array($result);
+
+        //$resultを使って$roles配列を作成
+        foreach ($result as $row) {
+            $roles[$row['id']] = $row['name'];
+        }
+        return $roles;
+    } catch (PDOException $e) {
+        exit('エラー: ' . $e->getMessage());
+    }
 }
