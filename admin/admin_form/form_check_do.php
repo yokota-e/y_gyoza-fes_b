72f375db71f6cd060d9d9176daa96b70f92bccd0
Tamaki Ryotaro <t.ryotaro.soccer.24@gmail.com>
2026-03-02 16:53:14 +0900

    お問い合わせ機能すべて完了

---
+++ admin/admin_form/form_check_do.php
@@ -0,0 +1,108 @@
+<!-- カンごみ：2026/03/02 記述 -->
+<!-- カンごみ：http://localhost:8080/y_gyoza-fes_b/admin/admin_form/form_list.php -->
+
+<?php
+require_once __DIR__ . '/../../function/function.php';
+require_once __DIR__ . '/../../common/login_check.php';
+?>
+
+
+
+<!-------------------------------------------------------------------------
+                            情報受け取り処理 
+------------------------------------------------------------------------->
+<?php
+//外部キー対応配列の取得
+$state_array = get_state_list();
+$user_array = get_users_list();
+?>
+
+
+
+
+<!-------------------------------------------------------------------------
+                    エラー処理と情報受け取り処理 
+------------------------------------------------------------------------->
+<?php
+//外部キー対応配列の取得
+$state_array = get_state_list();
+$user_array = get_users_list();
+//エラー用の変数宣言
+$error_message = "";
+$error_num = 0;
+
+if (empty($_POST)) {
+    $error_message = "POST通信ができていません";
+    $error_num = 1;
+}
+if (empty($_POST['id'])) {
+    $error_message = "IDが取得できていません";
+    $error_num = 2;
+} else {
+    $id = $_POST["id"];
+}
+if (empty($_POST["status"])) {
+    $error_message = "ステータス番号が取得できていません";
+    $error_num = 3;
+} else {
+    $staus_num = $_POST["status"];
+}
+if (empty($_SESSION["id"])) {
+    $error_message = "セッションの情報（ユーザーのID）を取得できていません";
+    $error_num = 4;
+} else {
+    $now_user_num = $_SESSION["id"];
+}
+?>
+<?php if ($error_num !== 0): ?>
+    <!DOCTYPE html>
+    <html lang="ja">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>【管理用】お問い合わせ担当編集</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css">
+        <link rel="stylesheet" href="./css/style.css">
+    </head>
+
+    <body>
+        <main>
+            <div class="card shadow mt-5">
+                <div class="card-header bg-danger text-white text-center fs-3">エラー！エラーナンバー：<?php echo $error_num ?></div>
+                <div class="card-body">
+                    <h2><?php echo $error_message ?></h2>
+                    <a href="form_list.php" class="btn btn-secondary me-3">
+                        一覧へ戻る
+                    </a>
+                </div>
+            </div>
+        </main>
+    </body>
+
+    </html>
+<?php endif; ?>
+
+
+
+
+<!-------------------------------------------------------------------------
+                        データベース変更処理
+------------------------------------------------------------------------->
+<?php
+if ($error_num == 0) {
+    try {
+        $db = db_connect();
+        $sql = 'UPDATE contacts SET support_date=NOW(), admin_name=:admin_name, status=:status WHERE id=:id';
+        $stmt = $db->prepare($sql);
+        $stmt->bindParam(':admin_name', $now_user_num, PDO::PARAM_INT);
+        $stmt->bindParam(':status', $staus_num, PDO::PARAM_INT);
+        $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+        $stmt->execute();
+    } catch (PDOException $e) {
+        exit('エラー（データベース変更時での）： ' . $e->getMessage());
+    }
+    header("location:form_list.php");
+    exit();
+}
+?>
---
+++ admin/admin_form/form_detail.php
@@ -0,0 +1,151 @@
+<!-- カンごみ：2026/03/02 記述 -->
+<!-- カンごみ：http://localhost:8080/y_gyoza-fes_b/admin/admin_form/form_list.php -->
+
+<?php
+require_once __DIR__ . '/../../function/function.php';
+require_once __DIR__ . '/../../common/login_check.php';
+?>
+
+
+
+<!-------------------------------------------------------------------------
+                            データベース処理
+------------------------------------------------------------------------->
+<?php
+$id = $_POST["id"];
+// // contactsテーブルからIDと対応したcontact情報の読み込み
+try {
+    // DBへ接続
+    $db = db_connect();
+    // プリペアードステートメント作成
+    $sql = 'SELECT * FROM contacts WHERE :id = id';
+    $stmt = $db->prepare($sql);
+    $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+    // SQLの実行
+    $stmt->execute();
+    $contact_datas = $stmt->fetch(PDO::FETCH_ASSOC);
+} catch (PDOException $e) {
+    exit('エラー（contactテーブルからのデータ取得時）:' . $e->getMessage());
+}
+
+//外部キー対応配列の取得
+$role_array = get_type_list();
+$state_array = get_state_list();
+$user_array = get_users_list();
+?>
+
+
+
+
+<!-------------------------------------------------------------------------
+                            HTML本文
+------------------------------------------------------------------------->
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>【管理用】お問い合わせ詳細</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+
+<body>
+
+    <h1 class="text-center m-5">お問い合わせ詳細</h1>
+    <main role="main" class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8 col-lg-6">
+                <div class="card shadow mt-5 mb-5">
+                    <div class="card-header bg-info text-white text-center fs-3">お問い合わせ詳細</div>
+                    <div class="card-body">
+                        <form method="POST" action="form_check_do.php" class="text-center">
+                            <dl class="row">
+                                <dt class="col-sm-4 text-center">ID</dt>
+                                <dd class="col-sm-8">
+                                    <?php echo ($contact_datas["id"]) ?>
+                                </dd>
+                                <hr>
+                                <dt class="col-sm-4 text-center">お問い合わせ種別</dt>
+                                <dd class="col-sm-8">
+                                    <?php echo ($role_array[$contact_datas["role"]]) ?>
+                                </dd>
+                                <hr>
+                                <dt class="col-sm-4 text-center">名前（ふりがな）</dt>
+                                <dd class="col-sm-8">
+                                    <?php echo ($contact_datas["name"] . "（" . $contact_datas["name_kana"] . "）") ?>
+                                </dd>
+                                <hr>
+                                <dt class="col-sm-4 text-center">会社名</dt>
+                                <dd class="col-sm-8">
+                                    <?php echo ($contact_datas["company"]) ?>
+                                </dd>
+                                <hr>
+                                <dt class="col-sm-4 text-center">電話番号</dt>
+                                <dd class="col-sm-8">
+                                    <?php echo ($contact_datas["tel"]) ?>
+                                </dd>
+                                <hr>
+                                <dt class="col-sm-4 text-center">メールアドレス</dt>
+                                <dd class="col-sm-8">
+                                    <?php echo ($contact_datas["address"]) ?>
+                                </dd>
+                                <hr>
+                                <dt class="col-sm-4 text-center">店名・媒体名</dt>
+                                <dd class="col-sm-8">
+                                    <?php echo ($contact_datas["shop_name"]) ?>
+                                </dd>
+                                <hr>
+                                <dt class="col-sm-4 text-center">お問い合わせ内容</dt>
+                                <dd class="col-sm-8">
+                                    <?php echo ($contact_datas["body"]) ?>
+                                </dd>
+                                <hr>
+                                <dt class="col-sm-4 text-center">投稿日時</dt>
+                                <dd class="col-sm-8">
+                                    <?php echo ($contact_datas["post_date"]) ?>
+                                </dd>
+                                <hr>
+                                <dt class="col-sm-4 text-center">対応日時</dt>
+                                <dd class="col-sm-8">
+                                    <?php echo ($contact_datas["support_date"]) ?>
+                                </dd>
+                                <hr>
+                                <dt class="col-sm-4 text-center">対応者</dt>
+                                <dd class="col-sm-8">
+                                    <?php echo ($user_array[$contact_datas["admin_name"]]) ?>
+                                </dd>
+                                <hr>
+                                <dt class="col-sm-4 text-center">対応状況</dt>
+                                <dd class="col-sm-8">
+                                    <?php echo ($state_array[$contact_datas["status"]]) ?>
+                                    <label for="status_select" class="form-label"></label>
+                                    <select name="status" class="form-select" id="status_select">
+                                        <!-- kan_to_do：あらかじめ選んでいたものが選ばれるように！ -->
+                                        <option disabled selected>変更する内容を選んでください</option>
+                                        <option value="1">未対応</option>
+                                        <option value="2">対応中</option>
+                                        <option value="3">対応済み</option>
+                                    </select>
+                                </dd>
+                                <hr>
+                            </dl>
+
+                            <a href="form_list.php" class="btn btn-secondary mt-2 mb-4 me-5">
+                                一覧へ戻る
+                            </a>
+
+                            <input type="hidden" name="id" value="<?php echo $id ?>">
+                            <input type="submit" class="btn btn-info mb-3 me-5" value="変更確定">
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </main>
+
+</body>
+
+</html>
