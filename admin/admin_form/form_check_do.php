72538f7b64a7ad0d5a397c759ed1dcec60f68d15
Tamaki Ryotaro <t.ryotaro.soccer.24@gmail.com>
2026-03-03 10:56:32 +0900

    メニューリストページ完成

--- admin/admin_form/form_check_do.php
+++ admin/admin_form/form_check_do.php
@@ -1,23 +1,10 @@
-<!-- カンごみ：2026/03/02 記述 -->
-<!-- カンごみ：http://localhost:8080/y_gyoza-fes_b/admin/admin_form/form_list.php -->
+<!-- カン：2026/03/02 記述 -->
+<!-- http://localhost:8080/y_gyoza-fes_b/admin/admin_form/form_list.php -->
 
 <?php
 require_once __DIR__ . '/../../function/function.php';
 require_once __DIR__ . '/../../common/login_check.php';
 ?>
-
-
-
-<!-------------------------------------------------------------------------
-                            情報受け取り処理 
-------------------------------------------------------------------------->
-<?php
-//外部キー対応配列の取得
-$state_array = get_state_list();
-$user_array = get_users_list();
-?>
-
-
 
 
 <!-------------------------------------------------------------------------
