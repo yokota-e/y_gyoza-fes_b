fcf637a409d815dd0ab0f7d43450780d43f617a8
Tamaki Ryotaro <t.ryotaro.soccer.24@gmail.com>
2026-03-04 12:49:43 +0900

    メニュー詳細完了

--- admin/admin_menu/menu_add.php
+++ admin/admin_menu/menu_add.php
@@ -50,7 +50,7 @@ require_once __DIR__ . '/../../common/login_check.php';
             </form>
 
             <footer class="text-center m-5 d-flex justify-content-center gap-5">
-                <a href="./shops_list.php" class="btn btn-primary">店舗一覧に戻る</a>
+                <a href="./menu_list.php" class="btn btn-primary">メニュー一覧に戻る</a>
                 <a href="../admin.php" class="btn btn-primary">管理者TOPに戻る</a>
             </footer>
             <!-- 本文ここまで -->
