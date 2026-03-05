14deb6e5ef152143cdc7944364b544a5e66a643b
Erika Yokota <rin0000ki@gmail.com>
2026-03-05 10:09:16 +0900

    店舗管理　復元機能追加

--- admin/admin_shops/shops_list.php
+++ admin/admin_shops/shops_list.php
@@ -30,7 +30,7 @@ try {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>【管理用】店舗一覧</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="./css/style.css">
+
 </head>
 
 <body>
